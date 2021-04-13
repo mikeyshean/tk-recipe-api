af13622fd70fec919da70cfb1f902cd9b95b3af6
mikeyshean <mdshean2@gmail.com>
2021-04-13 13:36:59 -0700

    Add initial recipe model and test

--- app/core/models.py
+++ app/core/models.py
@@ -7,3 +7,13 @@ class Ingredient(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Recipe(models.Model):
+    """Recipe object"""
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    ingredients = models.ManyToManyField('Ingredient')
+
+    def __str__(self):
+        return self.name
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,9 +6,17 @@ from core import models
 class ModelTests(TestCase):
 
     def test_ingredient_str(self):
-        """Test the ingredient stringn representation"""
+        """Test the ingredient string representation"""
         ingredient = models.Ingredient.objects.create(
             name="Cucumber"
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """Test the recipe string representation"""
+        recipe = models.Recipe.objects.create(
+            name="Pizza",
+            description="Cheesy Goodness",
+        )
+        self.assertEqual(str(recipe), recipe.name)
